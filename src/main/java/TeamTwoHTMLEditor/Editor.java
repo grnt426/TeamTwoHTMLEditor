c42f9009f94ad1d37d64086052a707375e999fc4
Kocsen Chung <kocsenc@gmail.com>
2013-03-21 11:46:52 -0400

    Adding Editor Class. Could be the model part of it and it can have a controller.

--- src/main/java/TeamTwoHTMLEditor/Editor.java
+++ src/main/java/TeamTwoHTMLEditor/Editor.java
@@ -5,9 +5,28 @@ package TeamTwoHTMLEditor;
  * User: Kocsen
  * Date: 3/21/13
  * Time: 10:00 AM
- * To change this template use File | Settings | File Templates.
+ * This is the model for the editor
  */
 public class Editor {
     private int numOpenFiles, numUnsavedFiles;
+    private boolean savePending;
+    private static Editor instance;
+
+    static {
+        instance = new Editor();
+    }
+
+    public static Editor getInstance() {
+        return instance;
+    }
+
+    /**
+     * Constructor for the Editor Class.
+     * Should be a singleton
+     */
+    public Editor() {
+
+    }
+
 
 }
