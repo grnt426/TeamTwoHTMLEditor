8e03e5a10bfb5c51cb0a06ef13eef3100fd5df43
Kocsen Chung <kocsenc@gmail.com>
2013-03-21 10:01:36 -0400

    Adding Editor Class. Could be the model part of it and it can have a controller.

---
+++ src/main/java/TeamTwoHTMLEditor/Editor.java
@@ -0,0 +1,13 @@
+package TeamTwoHTMLEditor;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Kocsen
+ * Date: 3/21/13
+ * Time: 10:00 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Editor {
+    private int numOpenFiles, numUnsavedFiles;
+
+}
