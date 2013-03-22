9ed94ced469949f80bd387dc021b14b492cb75de
Kocsen Chung <kocsenc@gmail.com>
2013-03-22 16:37:14 -0400

    button

---
+++ src/main/java/TeamTwoHTMLEditor/GUI/CloseTabButton.java
@@ -0,0 +1,27 @@
+package TeamTwoHTMLEditor.GUI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Keegan
+ * Date: 3/22/13
+ * Time: 4:23 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CloseTabButton extends JButton implements ActionListener {
+
+    public CloseTabButton() {
+        int size = 20;
+        setPreferredSize(new Dimension(size, size));
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
