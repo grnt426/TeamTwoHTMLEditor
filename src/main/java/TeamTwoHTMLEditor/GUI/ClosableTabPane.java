3cb5d5daf9e556c1d9954c53e5c70918cd43b19f
Keegan Parrotte <kmp3325@rit.edu>
2013-03-22 16:34:20 -0400

    Changed size so that it doesn't go off screen for some.

---
+++ src/main/java/TeamTwoHTMLEditor/GUI/ClosableTabPane.java
@@ -0,0 +1,16 @@
+package TeamTwoHTMLEditor.GUI;
+
+import javax.swing.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Keegan
+ * Date: 3/22/13
+ * Time: 4:23 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ClosableTabPane {
+	private final JTabbedPanePane pane;
+
+
+}
