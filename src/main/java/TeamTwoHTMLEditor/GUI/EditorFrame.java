2492937840207ab666df45291f12aa837b18d806
Grant Kurtz <th3guys2@gmail.com>
2013-03-24 19:55:53 -0400

    Fixed a mistake during the merge.

--- src/main/java/TeamTwoHTMLEditor/GUI/EditorFrame.java
+++ src/main/java/TeamTwoHTMLEditor/GUI/EditorFrame.java
@@ -285,7 +285,7 @@ public class EditorFrame extends JFrame {
         tabPane.setSelectedIndex(tabPane.getTabCount() - 1);
         newFileCount++;
 
-        new OpenCommand(f, pane).execute(commandDistributor);
+        new OpenCommand(f, pane, this).execute(commandDistributor);
     }
 
 	//What to do when a tab is selected
