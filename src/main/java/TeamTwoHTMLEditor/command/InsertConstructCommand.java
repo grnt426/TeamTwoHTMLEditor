80f5e6d04482e1b12342a3bd873c7153aa6fad91
Kocsen Chung <kocsenc@gmail.com>
2013-03-25 12:31:23 -0400

    Added cinsert construct command stubs to fill out. using enums

--- src/main/java/TeamTwoHTMLEditor/command/InsertConstructCommand.java
+++ src/main/java/TeamTwoHTMLEditor/command/InsertConstructCommand.java
@@ -7,8 +7,50 @@ import TeamTwoHTMLEditor.CommandDistributor;
  */
 public class InsertConstructCommand implements Command {
 
+    public enum Construct {
+        HEADER, BOLD, ITALICS, LIST, TABLE
+    }
+
+    ;
+
+    Construct construct;
+    int row, col;
+
+
+    public InsertConstructCommand(Construct cn, int r, int c) {
+        construct = cn;
+        row = r;
+        col = c;
+    }
+
+    public InsertConstructCommand(Construct cn) {
+        this(cn, 0, 0);
+    }
+
     @Override
     public void execute(CommandDistributor c) {
+        switch (construct) {
+            case HEADER:
+                //TODO Fill out header handling
+                //insert header: <header> \n </header>
+
+            case BOLD:
+                //TODO fill out bold heandling
+                //insert bold: <b> </b>
+
+            case ITALICS:
+                //TODO fill out italics handling
+                //insert italics: <i></i>
+
+            case LIST:
+                //TODO fill out list handling
+                //call make list command?
+
+            case TABLE:
+                //TODO fill out taable handling
+                //call make table command?
+
+        }
 
     }
 }
