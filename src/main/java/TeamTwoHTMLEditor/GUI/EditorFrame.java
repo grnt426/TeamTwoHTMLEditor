248f3f74edab25ac5b9b59bbd0cf1604f5896c89
Grant Kurtz <th3guys2@gmail.com>
2013-03-26 01:52:36 -0400

    Had to fix a merge error where the functions I added got removed during the merge.

--- src/main/java/TeamTwoHTMLEditor/GUI/EditorFrame.java
+++ src/main/java/TeamTwoHTMLEditor/GUI/EditorFrame.java
@@ -568,5 +568,40 @@ public class EditorFrame extends JFrame {
     JTextArea getActivePane() {
         return editorPanes.get(activePane);
     }
+
+	public static int getTabCount(String str){
+		int tabCount = 0;
+		for(char c : str.toCharArray()){
+			if(c == '\t')
+				tabCount++;
+			else
+				break;
+		}
+
+		return tabCount;
+	}
+
+	public static String getCurrentLine(JTextArea pane){
+		int index = 0;
+		try{
+			index = pane.getLineOfOffset(pane.getCaretPosition());
+		}
+		catch(BadLocationException e){
+			// Not sure if we can do much.  Assume that there is no cursor
+			// and that we don't need to auto-indent.
+			return null;
+		}
+
+		String[] content = pane.getText().split("\n");
+		return content[index-1];
+	}
+
+	public static String indentTabs(int tabCount){
+		String tabs = "";
+		for(; tabCount > 0; tabCount--){
+			 tabs += "\t";
+		}
+		return tabs;
+	}
 }
 
