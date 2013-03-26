7850101ae0bea0065ff102ab185f3a196291e834
Grant Kurtz <th3guys2@gmail.com>
2013-03-26 15:08:05 -0400

    Cleaned up inserting lists and implemented the other two.

--- src/main/java/TeamTwoHTMLEditor/command/InsertListCommand.java
+++ src/main/java/TeamTwoHTMLEditor/command/InsertListCommand.java
@@ -8,65 +8,58 @@ import javax.swing.*;
 /**
  * For inserting List elements.
  */
-public class InsertListCommand implements Command {
+public class InsertListCommand implements Command{
 
 	private final JTextArea activePane;
 
-	public enum ListType {NUMBERED, BULLETED, DICTIONARY}
+	public enum ListType{NUMBERED, BULLETED, DICTIONARY}
 
 	int size;
-    ListType type;
+	ListType type;
 
 
-    public InsertListCommand(ListType l, int size, JTextArea activePane) {
-        this.size = size;
-        type = l;
+	public InsertListCommand(ListType l, int size, JTextArea activePane){
+		this.size = size;
+		type = l;
 		this.activePane = activePane;
 	}
 
-    @Override
-    public void execute(CommandDistributor c) {
+	@Override
+	public void execute(CommandDistributor c){
 		StringBuilder listElement = new StringBuilder("");
-		int tabCount = EditorFrame.getTabCount(EditorFrame.getCurrentLine(activePane));
+		int tabCount =
+				EditorFrame.getTabCount(EditorFrame.getCurrentLine(activePane));
 		String tabs = EditorFrame.indentTabs(tabCount);
 		System.out.println(tabCount);
 
-        switch (type) {
-            case NUMBERED:
-            /*
-            Example:
-            <ol>
-                <li>  text here </li>
-                <li>   more text here </li>
-            </ol>
-             */
-			listElement.append("<ol>\n");
-			while(size-- > 0) listElement.append(tabs).append("\t<li></li>\n");
-			listElement.append(tabs).append("</ol>");
-			break;
+		switch(type){
+			case NUMBERED:
+				listElement.append("<ol>\n");
+				while(size-- > 0){
+					listElement.append(tabs).append("\t<li></li>\n");
+				}
+				listElement.append(tabs).append("</ol>");
+				break;
 
-            case BULLETED:
-            /* Example: (tabs or no tabs after <ul>?)
-            <ul>
-                <li>text</li>
-                <li>text</li>
-            </ul>
-             */
+			case BULLETED:
+				listElement.append("<ul>\n");
+				while(size-- > 0){
+					listElement.append(tabs).append("\t<li></li>\n");
+				}
+				listElement.append(tabs).append("</ul>");
+				break;
 
-
-            case DICTIONARY:
-            /*
-            <dl>
-            <dt>Coffee</dt>
-            <dd>black hot drink</dd>
-            <dt>Milk</dt>
-            <dd>white cold drink</dd>
-            </dl>
-             */
-
-        }
+			case DICTIONARY:
+				listElement.append("<dl>\n");
+				while(size-- > 0){
+					listElement.append(tabs).append("\t<dt></dt>\n");
+					listElement.append(tabs).append("\t<dd></dd>\n");
+				}
+				listElement.append(tabs).append("</dl>");
+				break;
+		}
 		System.out.println(listElement.toString());
 		activePane.insert(listElement.toString(), activePane.getCaretPosition());
-    }
+	}
 
 }
