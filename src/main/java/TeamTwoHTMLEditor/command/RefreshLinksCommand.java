9af317d7438103a8e4da875e3395bb9d7a131560
Shannon Trudeau <smt9020@rit.edu>
2013-04-04 15:10:16 -0400

    added a couple command skeletons

---
+++ src/main/java/TeamTwoHTMLEditor/command/RefreshLinksCommand.java
@@ -0,0 +1,19 @@
+package TeamTwoHTMLEditor.command;
+
+import TeamTwoHTMLEditor.CommandDistributor;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Shannon
+ * Date: 4/4/13
+ * Time: 2:40 PM
+ *
+ */
+public class RefreshLinksCommand implements Command {
+
+
+    @Override
+    public void execute(CommandDistributor c) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
---
+++ src/main/java/TeamTwoHTMLEditor/command/RenderPreviewCommand.java
@@ -0,0 +1,21 @@
+package TeamTwoHTMLEditor.command;
+
+import TeamTwoHTMLEditor.CommandDistributor;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Shannon
+ * Date: 4/4/13
+ * Time: 2:43 PM
+ *
+ */
+public class RenderPreviewCommand implements Command{
+
+
+
+
+    @Override
+    public void execute(CommandDistributor c) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
