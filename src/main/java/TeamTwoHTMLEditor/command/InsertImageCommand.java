b3a8bcbc1fc0453c3d195b95fbc433e9d7959417
Kocsen Chung <kocsenc@gmail.com>
2013-04-13 15:32:40 -0400

    Comment

--- src/main/java/TeamTwoHTMLEditor/command/InsertImageCommand.java
+++ src/main/java/TeamTwoHTMLEditor/command/InsertImageCommand.java
@@ -5,7 +5,7 @@ package TeamTwoHTMLEditor.command;
  * User: Shannon
  * Date: 4/5/13
  * Time: 3:31 PM
- * To change this template use File | Settings | File Templates.
+ * Command to insert an image. Gets info from popped up gui and then inserts the string into the active pane.
  */
 
 import TeamTwoHTMLEditor.CommandDistributor;
@@ -13,27 +13,27 @@ import TeamTwoHTMLEditor.CommandMediator;
 
 import javax.swing.*;
 
-public class InsertImageCommand implements Command{
+public class InsertImageCommand implements Command {
 
-	private final JTextArea activePane;
-	private String src;
+    private final JTextArea activePane;
+    private String src;
 
-	public InsertImageCommand(String src, JTextArea activePane){
-		this.src = src;
-		this.activePane = activePane;
-	}
+    public InsertImageCommand(String src, JTextArea activePane) {
+        this.src = src;
+        this.activePane = activePane;
+    }
 
-	public void execute(CommandDistributor c, CommandMediator cmd){
-		String input;
+    public void execute(CommandDistributor c, CommandMediator cmd) {
+        String input;
 
-		// If there is no active editor window, then do nothing
-		if(activePane == null){
-			return;
-		}
-		input = "<img src=\"" + src + "\">";
-		StringBuilder insertStr = new StringBuilder("");
-		insertStr.append(input);
+        // If there is no active editor window, then do nothing
+        if (activePane == null) {
+            return;
+        }
+        input = "<img src=\"" + src + "\">";
+        StringBuilder insertStr = new StringBuilder("");
+        insertStr.append(input);
 
-		activePane.insert(insertStr.toString(), activePane.getCaretPosition());
-	}
+        activePane.insert(insertStr.toString(), activePane.getCaretPosition());
+    }
 }
