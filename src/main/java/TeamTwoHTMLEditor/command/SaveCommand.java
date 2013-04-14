2ee484620308ab401b3dbf26801e176aae5cc1e5
Kocsen Chung <kocsenc@gmail.com>
2013-04-14 17:32:53 -0400

    Save Command arrangement fix

--- src/main/java/TeamTwoHTMLEditor/command/SaveCommand.java
+++ src/main/java/TeamTwoHTMLEditor/command/SaveCommand.java
@@ -2,28 +2,24 @@ package TeamTwoHTMLEditor.command;
 
 import TeamTwoHTMLEditor.CommandDistributor;
 import TeamTwoHTMLEditor.CommandMediator;
-import TeamTwoHTMLEditor.GUI.EditorFrame;
-
-import javax.swing.*;
 
 /**
  * Handles delegating the task of saving.
  */
-public class SaveCommand implements Command{
+public class SaveCommand implements Command {
 
-	private final ActiveContext context;
+    private final ActiveContext context;
 
-	public SaveCommand(ActiveContext context){
-		this.context = context;
-	}
+    public SaveCommand(ActiveContext context) {
+        this.context = context;
+    }
 
-	@Override
-	public void execute(CommandDistributor c, CommandMediator cmd){
-		String contents = context.getActiveTextArea().getText();
-		String path = c.getFileManager().getPathAt(context.getIndex());
-		c.getFileManager().quickSaveFile(path, contents, context.getIndex());
-		c.getFileManager().printStatus();
-		cmd.saveCommandExecuted(path, false, c, context);
-		c.getFileManager().printStatus();
-	}
+    @Override
+    public void execute(CommandDistributor c, CommandMediator cmd) {
+        String contents = context.getActiveTextArea().getText();
+        String path = c.getFileManager().getPathAt(context.getIndex());
+        c.getFileManager().quickSaveFile(path, contents, context.getIndex());
+        cmd.saveCommandExecuted(path, false, c, context);
+        c.getFileManager().printStatus();
+    }
 }
